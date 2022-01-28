13d016ea1dde06fe85dfb133ce75175306ebd776
Luke <shijiluke@gmail.com>
2022-01-28 19:29:21 +0800

    first push

---
+++ main.py
@@ -0,0 +1,5 @@
+import yahoo_fin.stock_info as si
+
+def getBookvalue():
+    print("this")
+
---
+++ p1.py
@@ -0,0 +1,64 @@
+import yahoo_fin.stock_info as si
+
+
+def fo(number):
+    return str(f"{number:,}")
+
+def main():
+    stockName = "BABA"
+    bs = si.get_balance_sheet(stockName)
+    cf = si.get_cash_flow(stockName)
+    incomeStatement = si.get_income_statement(stockName)
+
+    info = si.get_company_info(stockName)
+
+    country = info.loc["country"][0]
+    sector= info.loc['sector'][0]
+
+    revenue = incomeStatement.loc["totalRevenue"][0]
+
+    retainedEarnings = bs.loc["retainedEarnings"][0]
+    equity = bs.loc["totalStockholderEquity"][0]
+    cfo = cf.loc["totalCashFromOperatingActivities"][0]
+    cfi = cf.loc["totalCashflowsFromInvestingActivities"][0]
+    cff = cf.loc["totalCashFromFinancingActivities"][0]
+
+    marketPrice = si.get_live_price(stockName)
+    shares = si.get_quote_data(stockName)['sharesOutstanding']
+
+    totalCurrentAssets= bs.loc["totalCurrentAssets"][0]
+    totalCurrentLiab = bs.loc["totalCurrentLiabilities"][0]
+
+    totalAssets = bs.loc["totalAssets"][0]
+    totalLiab = bs.loc["totalLiab"][0]
+
+    ebit = incomeStatement.loc["ebit"][0]
+
+    #PRINTING*****
+    print(stockName,country, sector)
+    print("shares",shares/1000000000.0,"B")
+    print("A",totalAssets/1000000000,"(", totalCurrentAssets/1000000000.0
+          ,(totalAssets-totalCurrentAssets)/1000000000.0,")")
+    print("L",totalLiab/1000000000,"(", totalCurrentLiab/1000000000.0,
+          (totalLiab-totalCurrentLiab)/1000000000.0,")")
+    print("E", (totalAssets-totalLiab) / 1000000000.0, "B")
+    print("market cap USD", marketPrice*shares/1000000000.0,"B")
+    print("shareholder equity", equity/1000000000.0, "B")
+
+    #print("P/B", marketPrice*shares/equity)
+    print("                         ")
+    print("********ALTMAN**********")
+    print("current ratio", totalCurrentAssets/totalCurrentLiab)
+    print("debt equity ratio", totalLiab/(totalAssets-totalLiab))
+    print("ebit", ebit/1000000000,"B")
+    print("CFO", cfo/1000000000,"B")
+    print("CFI", cfi/1000000000,"B")
+    print("CFF", cff / 1000000000, "B")
+    print("retailed earnings", retainedEarnings/1000000000,"B")
+    print("retailed earnings/totalAssets", retainedEarnings/totalAssets)
+    print("revenue/totalAssets",revenue/totalAssets)
+
+
+
+if __name__ == "__main__":
+    main()
