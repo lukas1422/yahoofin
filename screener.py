0602d7a05ad2a2063d5ae6d18672ad51ff308d83
Luke <shijiluke@gmail.com>
2022-01-29 08:43:12 +0800

    1 29 (3rd)

--- screener.py
+++ screener.py
@@ -14,22 +14,33 @@ def main():
         if (country.lower()) == "china":
             print(comp, "NO CHINA")
         else:
-            #print(comp, country)
+            # print(comp, country)
             bs = si.get_balance_sheet(comp)
+            cf = si.get_cash_flow(comp)
+            incomeStatement = si.get_income_statement(comp)
+
             retainedEarnings = bs.loc["retainedEarnings"][0]
             equity = bs.loc["totalStockholderEquity"][0]
-            totalCurrentAssets= bs.loc["totalCurrentAssets"][0]
+            totalCurrentAssets = bs.loc["totalCurrentAssets"][0]
             totalCurrentLiab = bs.loc["totalCurrentLiabilities"][0]
             totalAssets = bs.loc["totalAssets"][0]
             totalLiab = bs.loc["totalLiab"][0]
 
+            # IS
+            revenue = incomeStatement.loc["totalRevenue"][0]
+            EBIT = incomeStatement.loc["ebit"][0]
+
+            # CF
+            cfo = cf.loc["totalCashFromOperatingActivities"][0]
+            cfi = cf.loc["totalCashflowsFromInvestingActivities"][0]
+            cff = cf.loc["totalCashFromFinancingActivities"][0]
+
             print(comp, country,
-                  "CR",round(totalCurrentAssets/totalCurrentLiab,2),
-                  "D/E",round(totalLiab/(totalAssets-totalLiab),2),
-                  "RE/A",round(retainedEarnings/totalAssets,2))
-
-
-
+                  "CR", round(totalCurrentAssets / totalCurrentLiab, 2),
+                  "D/E", round(totalLiab / (totalAssets - totalLiab), 2),
+                  "RE/A", round(retainedEarnings / totalAssets, 2),
+                  "cfo/A", round(cfo / totalAssets, 2),
+                  "EBIT/A", round(EBIT / totalAssets, 2))
 
 
 if __name__ == "__main__":
