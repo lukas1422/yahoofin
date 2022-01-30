17ac06f4e675ea00975e923cb08706f6064f8b98
Luke <shijiluke@gmail.com>
2022-01-30 12:57:06 +0800

    1 29 (3rd)

--- scrapeFinviz.py
+++ scrapeFinviz.py
@@ -7,6 +7,9 @@
 
 ########
 
+fileOutput = open('tickerList', 'w')
+# fileOutput.write("\n")
+
 import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
@@ -22,12 +25,24 @@ soup = BeautifulSoup(webpage, "html.parser")
 # for link in ab:
 #     print(link.get_text(strip=True))
 
+last = 0
 
 for i in range(1, stockNum, 20):
-   url = "http://finviz.com/screener.ashx?v=121&f=sec_industrials&o=pb&r=" + str(i)
-   req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-   webpage = urlopen(req).read()
-   soup = BeautifulSoup(webpage, "html.parser")
-   tBody = soup.find('div', attrs={"id": "screener-content"})
-   for tr in tBody.find_all('tr', attrs={'class': ["table-dark-row-cp", "table-light-row-cp"]}):
-       print(tr.find_all('td')[0].get_text(strip=True), tr.find_all('td')[1].get_text(strip=True))
+    url = "http://finviz.com/screener.ashx?v=121&f=sec_industrials&o=pb&r=" + str(i)
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    soup = BeautifulSoup(webpage, "html.parser")
+    tBody = soup.find('div', attrs={"id": "screener-content"})
+
+    for tr in tBody.find_all('tr', attrs={'class': ["table-dark-row-cp", "table-light-row-cp"]}):
+        index = tr.find_all('td')[0].get_text(strip=True)
+        value = tr.find_all('td')[1].get_text(strip=True)
+        print("index", index)
+        if index == last:
+            break
+        else:
+            print(index, value)
+            last = index
+            print("last", last)
+            fileOutput.write(str(value) + "\n")
+            fileOutput.flush()
--- screener.py
+++ screener.py
@@ -12,118 +12,123 @@ with open("tickerList", "r") as file:
 
 print(lines)
 for comp in lines:
-    info = si.get_company_info(comp)
-    country = info.loc["country"][0]
-    sector = info.loc['sector'][0]
-    bs = si.get_balance_sheet(comp)
-    # print(comp, country)
-    if (country.lower()) == "china":
-        print(comp, "NO CHINA")
-    # elif not ("retainedEarnings" in bs.index):
-    #     print(comp, "has no retained earnings")
+    try:
+        info = si.get_company_info(comp)
+        country = info.loc["country"][0]
+        sector = info.loc['sector'][0]
+        bs = si.get_balance_sheet(comp)
+
+    except Exception as e:
+        print(comp, "exception on info or BS")
     else:
         # print(comp, country)
-        # bs = si.get_balance_sheet(comp)
-
-        #
-        # if "retainedEarnings" in bs.index:
-        #     retainedEarnings = bs.loc["retainedEarnings"][0]
-        # else:
-        #     print("retained earnings does not exist for ", comp)
-        #     retainedEarnings = 0.0
-
-        try:
-            cf = si.get_cash_flow(comp)
-            incomeStatement = si.get_income_statement(comp)
-
-            equity = bs.loc["totalStockholderEquity"][0]
-            totalCurrentAssets = bs.loc["totalCurrentAssets"][0]
-            totalCurrentLiab = bs.loc["totalCurrentLiabilities"][0]
-            totalAssets = bs.loc["totalAssets"][0]
-            totalLiab = bs.loc["totalLiab"][0]
-            retainedEarnings = bs.loc["retainedEarnings"][0]
-
-            # IS
-            # revenue = incomeStatement.loc["totalRevenue"][0]
-            ebit = incomeStatement.loc["ebit"][0]
-
-            # CF
-            cfo = cf.loc["totalCashFromOperatingActivities"][0]
-            # cfi = cf.loc["totalCashflowsFromInvestingActivities"][0]
-            # cff = cf.loc["totalCashFromFinancingActivities"][0]
-            marketPrice = si.get_live_price(comp)
-            shares = si.get_quote_data(comp)['sharesOutstanding']
-        except Exception as e:
-            print("error when getting data ", comp, e)
+        if (country.lower()) == "china":
+            print(comp, "NO CHINA")
+        # elif not ("retainedEarnings" in bs.index):
+        #     print(comp, "has no retained earnings")
         else:
-            marketCap = marketPrice * shares
-            currentRatio = totalCurrentAssets / totalCurrentLiab
-            debtEquityRatio = totalLiab / (totalAssets - totalLiab)
-            retainedEarningsAssetRatio = retainedEarnings / totalAssets
-            cfoAssetRatio = cfo / totalAssets
-            ebitAssetRatio = ebit / totalAssets
+            # print(comp, country)
+            # bs = si.get_balance_sheet(comp)
+
+            #
+            # if "retainedEarnings" in bs.index:
+            #     retainedEarnings = bs.loc["retainedEarnings"][0]
+            # else:
+            #     print("retained earnings does not exist for ", comp)
+            #     retainedEarnings = 0.0
 
             try:
-                assert currentRatio > 1, 'current ratio needs to be bigger than one'
-            except AssertionError as ae:
-                print(comp, "fails current ratio", currentRatio, ae)
+                cf = si.get_cash_flow(comp)
+                incomeStatement = si.get_income_statement(comp)
 
-            try:
-                assert debtEquityRatio < 1, 'debt equity ratio needs to be less than one'
-            except AssertionError as ae:
-                print(comp, "fails DE ratio", debtEquityRatio, ae)
+                equity = bs.loc["totalStockholderEquity"][0]
+                totalCurrentAssets = bs.loc["totalCurrentAssets"][0]
+                totalCurrentLiab = bs.loc["totalCurrentLiabilities"][0]
+                totalAssets = bs.loc["totalAssets"][0]
+                totalLiab = bs.loc["totalLiab"][0]
+                retainedEarnings = bs.loc["retainedEarnings"][0]
 
-            try:
-                assert retainedEarnings > 0, "retained earnings needs to be greater than 0"
-            except AssertionError as ae:
-                print(comp, "fails retained earnings", retainedEarnings, ae)
+                # IS
+                # revenue = incomeStatement.loc["totalRevenue"][0]
+                ebit = incomeStatement.loc["ebit"][0]
 
-            try:
-                assert cfo > 0, "cfo needs to be greater than 0"
-            except AssertionError as ae:
-                print(comp, "fails CFO", cfo, ae)
-
-            try:
-                assert ebit > 0, "ebit needs to be postive"
-            except AssertionError as ae:
-                print(datetime.datetime.now().time().strftime("%H:%M"), comp, "fails EBIT", ebit, ae)
-
-            try:
-                assert currentRatio > 1, 'current ratio needs to be bigger than one'
-                assert debtEquityRatio < 1, 'debt equity ratio needs to be less than one'
-                assert retainedEarnings > 0, "retained earnings needs to be greater than 0"
-                assert cfo > 0, "cfo needs to be greater than 0"
-                assert ebit > 0, "ebit needs to be postive"
-            except AssertionError as ae:
-                print(comp, "fails assertion", ae)
+                # CF
+                cfo = cf.loc["totalCashFromOperatingActivities"][0]
+                # cfi = cf.loc["totalCashflowsFromInvestingActivities"][0]
+                # cff = cf.loc["totalCashFromFinancingActivities"][0]
+                marketPrice = si.get_live_price(comp)
+                shares = si.get_quote_data(comp)['sharesOutstanding']
+            except Exception as e:
+                print("error when getting data ", comp, e)
             else:
-                if (currentRatio > 1 and debtEquityRatio < 1 and retainedEarnings > 0
-                        and cfo > 0 and ebit > 0):
-                    pb = marketCap / equity
-                    data = si.get_data(comp, start_date=START_DATE, interval=PRICE_INTERVAL)
-                    # print("data ", data)
+                marketCap = marketPrice * shares
+                currentRatio = totalCurrentAssets / totalCurrentLiab
+                debtEquityRatio = totalLiab / (totalAssets - totalLiab)
+                retainedEarningsAssetRatio = retainedEarnings / totalAssets
+                cfoAssetRatio = cfo / totalAssets
+                ebitAssetRatio = ebit / totalAssets
 
-                    try:
-                        dataSize = data['adjclose'].size
-                        # print("data size is ", data.size)
-                        percentile = 100.0 * (data['adjclose'][-1] - data['adjclose'].min()) / (
-                                data['adjclose'].max() - data['adjclose'].min())
-                        # percentile = data['adjclose'].rank(method='max').apply(lambda x: 100 * (x - 1) / dataSize)[-1]
-                    except Exception as e:
-                        print(comp, "percentile issue ", e)
-                    else:
+                try:
+                    assert currentRatio > 1, 'current ratio needs to be bigger than one'
+                except AssertionError as ae:
+                    print(comp, "fails current ratio", currentRatio, ae)
 
-                        outputString = "SUCCESS " + comp + " " + country + " " + sector \
-                                       + " MV:" + str(round(marketCap / 1000000000.0, 1)) + 'B' \
-                                       + " Equity:" + str(round((totalAssets-totalLiab) / 1000000000.0, 1)) + 'B' \
-                                       + " CR:" + str(round(currentRatio, 1)) \
-                                       + " D/E:" + str(round(debtEquityRatio, 1)) \
-                                       + " RE/A:" + str(round(retainedEarningsAssetRatio, 1)) \
-                                       + " cfo/A:" + str(round(cfoAssetRatio, 1)) \
-                                       + " ebit/A:" + str(round(ebitAssetRatio, 1)) \
-                                       + " p/b:" + str(round(pb, 1)) \
-                                       + " 52wk p%: " + str(round(percentile))
+                try:
+                    assert debtEquityRatio < 1, 'debt equity ratio needs to be less than one'
+                except AssertionError as ae:
+                    print(comp, "fails DE ratio", debtEquityRatio, ae)
 
-                        print(outputString)
-                        fileOutput.write(outputString + '\n')
-                        fileOutput.flush()
+                try:
+                    assert retainedEarnings > 0, "retained earnings needs to be greater than 0"
+                except AssertionError as ae:
+                    print(comp, "fails retained earnings", retainedEarnings, ae)
+
+                try:
+                    assert cfo > 0, "cfo needs to be greater than 0"
+                except AssertionError as ae:
+                    print(comp, "fails CFO", cfo, ae)
+
+                try:
+                    assert ebit > 0, "ebit needs to be postive"
+                except AssertionError as ae:
+                    print(datetime.datetime.now().time().strftime("%H:%M"), comp, "fails EBIT", ebit, ae)
+
+                try:
+                    assert currentRatio > 1, 'current ratio needs to be bigger than one'
+                    assert debtEquityRatio < 1, 'debt equity ratio needs to be less than one'
+                    assert retainedEarnings > 0, "retained earnings needs to be greater than 0"
+                    assert cfo > 0, "cfo needs to be greater than 0"
+                    assert ebit > 0, "ebit needs to be postive"
+                except AssertionError as ae:
+                    print(comp, "fails assertion", ae)
+                else:
+                    if (currentRatio > 1 and debtEquityRatio < 1 and retainedEarnings > 0
+                            and cfo > 0 and ebit > 0):
+                        pb = marketCap / equity
+                        data = si.get_data(comp, start_date=START_DATE, interval=PRICE_INTERVAL)
+                        # print("data ", data)
+
+                        try:
+                            dataSize = data['adjclose'].size
+                            # print("data size is ", data.size)
+                            percentile = 100.0 * (data['adjclose'][-1] - data['adjclose'].min()) / (
+                                    data['adjclose'].max() - data['adjclose'].min())
+                            # percentile = data['adjclose'].rank(method='max').apply(lambda x: 100 * (x - 1) / dataSize)[-1]
+                        except Exception as e:
+                            print(comp, "percentile issue ", e)
+                        else:
+
+                            outputString = "SUCCESS " + comp + " " + country + " " + sector \
+                                           + " MV:" + str(round(marketCap / 1000000000.0, 1)) + 'B' \
+                                           + " Equity:" + str(round((totalAssets-totalLiab) / 1000000000.0, 1)) + 'B' \
+                                           + " CR:" + str(round(currentRatio, 1)) \
+                                           + " D/E:" + str(round(debtEquityRatio, 1)) \
+                                           + " RE/A:" + str(round(retainedEarningsAssetRatio, 1)) \
+                                           + " cfo/A:" + str(round(cfoAssetRatio, 1)) \
+                                           + " ebit/A:" + str(round(ebitAssetRatio, 1)) \
+                                           + " p/b:" + str(round(pb, 1)) \
+                                           + " 52wk p%: " + str(round(percentile))
+
+                            print(outputString)
+                            fileOutput.write(outputString + '\n')
+                            fileOutput.flush()
