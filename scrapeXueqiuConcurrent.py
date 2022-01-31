011e4f89cf120ce0c526ec4a0a317d18747d947e
Luke <shijiluke@gmail.com>
2022-01-31 10:05:00 +0800

    1 31 1004

--- scrapeXueqiuConcurrent.py
+++ scrapeXueqiuConcurrent.py
@@ -7,37 +7,59 @@ with open("usTickerAll", "r") as file:
 
 MAX_THREADS = 30
 
-urlList = []
-urlDict = {}
+companyList = []
+# urlDict = {}
 tickerPBDict = {}
 
 fileOutput = open('test', 'w')
 
+numberProcessed = 0
 
-def processFunction(url):
+
+def increment():
+    global numberProcessed
+    numberProcessed = numberProcessed + 1
+
+
+def processFunction(comp, url):
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req).read()
     soup = BeautifulSoup(webpage, "html.parser")
 
     for a in soup.find_all('td'):
         if a.getText().startswith("市净率"):
-            # print(" processed ", comp, a.find('span').getText())
-            return comp + " " + a.find('span').getText()
+            increment()
+            print(numberProcessed, " processed ", comp, a.find('span').getText())
+            return a.find('span').getText()
+            # return comp + " " + a.find('span').getText()
+
+def makeURL(compName):
+    return "https://xueqiu.com/S/" + compName
 
 
 ######### MAIN ############
 for comp in lines:
+    companyList.append(comp)
     # urlDict[comp] = "https://xueqiu.com/S/" + comp
-    urlList.append("https://xueqiu.com/S/" + comp)
+    # urlList.append("https://xueqiu.com/S/" + comp)
+
+# print("printing URLLIST",urlList)
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
-    future_to_pb = [executor.submit(processFunction, url) for url in urlList]
+    compToPBFuture = {executor.submit(processFunction, comp, makeURL(comp)): comp for comp in companyList}
 
-    for future in concurrent.futures.as_completed(future_to_pb):
+    for future in concurrent.futures.as_completed(compToPBFuture):
         try:
+            comp = compToPBFuture[future]
             data = future.result()
         except Exception as e:
-            print("exception ", e)
+            print("exception ", comp, e)
         else:
-            fileOutput.write(data + "\n")
-            fileOutput.flush()
+            tickerPBDict[comp] = data
+            # fileOutput.write(comp + " " + data + "\n")
+            # fileOutput.flush()
+
+for comp in tickerPBDict.keys():
+    fileOutput.write(comp + " " + data + "\n")
+
+fileOutput.flush()
