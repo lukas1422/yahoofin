ce51a12b19edbf46cac65b212df15a3dede44e53
Luke <shijiluke@gmail.com>
2022-02-04 06:27:52 +0800

    2 4 0627

--- scrapeFinvizNewLowTopLoser.py
+++ scrapeFinvizNewLowTopLoser.py
@@ -4,12 +4,13 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 
 
-
-def finvizToFile(url, numStocks):
-    last = 0
+def finvizToFile(urlBase, numStocks):
+    # last = 0
     for i in range(1, numStocks, 20):
-        url = url + str(i)
-        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        urlToScrape = urlBase + str(i)
+        print("i url ", i, urlToScrape)
+
+        req = Request(urlToScrape, headers={'User-Agent': 'Mozilla/5.0'})
         webpage = urlopen(req).read()
         soup = BeautifulSoup(webpage, "html.parser")
         tBody = soup.find('div', attrs={"id": "screener-content"})
@@ -19,13 +20,13 @@ def finvizToFile(url, numStocks):
             value = tr.find_all('td')[1].get_text(strip=True)
             # pb = tr.find_all('td')[7].get_text(strip=True)
             print("index", index, "value", value)
-            if index == last:
-                break
-            else:
-                print(index, value)
-                last = index
-                fileOutput.write(str(value) + "\n")
-                fileOutput.flush()
+            # if index == last:
+            #     break
+            # else:
+            print(index, value)
+            # last = index
+            fileOutput.write(str(value) + "\n")
+            fileOutput.flush()
 
 
 finvizToFile('https://finviz.com/screener.ashx?v=171&o=low52w&r=', 200)
