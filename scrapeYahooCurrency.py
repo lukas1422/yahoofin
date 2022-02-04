2e0660945aff8ded54f5ef68e815bd0abfb4f2bc
Luke <shijiluke@gmail.com>
2022-02-04 13:00:19 +0800

    2 4 1300

--- scrapeYahooCurrency.py
+++ scrapeYahooCurrency.py
@@ -1,24 +1,20 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 
-url = "https://finance.yahoo.com/quote/0743.HK/financials?p=0743.HK"
-url2 = "https://finance.yahoo.com/quote/baba/financials?p=baba"
+
+def yahooBalanceSheetURLMaker(ticker, type):
+    return "https://finance.yahoo.com/quote/" + ticker + "/" + type + "?p=" + ticker
 
 
-def yahooFinancialURLMaker(ticker):
-    return "https://finance.yahoo.com/quote/" + ticker + "/financials?p=" + ticker
-
-
-# try:
-# url = "https://xueqiu.com/S/" + comp
 try:
-    comp = "1024.HK"
-    req = Request(yahooFinancialURLMaker(comp), headers={'User-Agent': 'Mozilla/5.0'})
+    comp = "PDD"
+    type = "history"
+    req = Request(yahooBalanceSheetURLMaker(comp, type), headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req).read()
     soup = BeautifulSoup(webpage, "html.parser")
     for a in soup.find_all('span'):
         if (a.getText().startswith("Currency in")):
-            print(comp, a.getText())
+            print(comp, type, a.getText())
 
     # print(soup)
 except Exception as e:
