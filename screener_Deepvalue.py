c46ceab828c244cf65c299e6922990f1a8d7a9ee
Luke <shijiluke@gmail.com>
2022-02-05 21:57:48 +0800

    2 5 2157

--- screener_Deepvalue.py
+++ screener_Deepvalue.py
@@ -43,96 +43,139 @@ for comp in lines:
         country = info.loc["country"][0]
         sector = info.loc['sector'][0]
         bs = si.get_balance_sheet(comp)
+
+        if country.lower() == "china":
+            print(comp, "no china")
+            continue
+
+        totalCurrentAssets = getFromDF(bs.loc["totalCurrentAssets"])
+        totalCurrentLiab = getFromDF(bs.loc["totalCurrentLiabilities"])
+        currentRatio = totalCurrentAssets / totalCurrentLiab
+
+        if currentRatio < 1:
+            print(comp, "current ratio < 1")
+            continue
+
+        retainedEarnings = getFromDF(bs.loc["retainedEarnings"])
+
+        if retainedEarnings < 0:
+            print(comp, " retained earnings < 0 ", retainedEarnings)
+            continue
+
+        totalAssets = getFromDF(bs.loc["totalAssets"])
+        totalLiab = getFromDF(bs.loc["totalLiab"])
+        debtEquityRatio = totalLiab / (totalAssets - totalLiab)
+
+        if debtEquityRatio > 1:
+            print(comp, " de ratio> 1. ", debtEquityRatio)
+            continue
+
+        incomeStatement = si.get_income_statement(comp, yearly=True)
+        ebit = getFromDF(incomeStatement.loc["ebit"])
+        netIncome = getFromDF(incomeStatement.loc['netIncome'])
+
+        if ebit < 0 or netIncome < 0:
+            print(comp, "ebit or net income < 0 ", ebit, " ", netIncome)
+            continue
+
+        cf = si.get_cash_flow(comp)
+        cfo = getFromDF(cf.loc["totalCashFromOperatingActivities"])
+
+        if cfo < 0:
+            print(comp, "cfo < 0 ", cfo)
+            continue
+
+        equity = getFromDF(bs.loc["totalStockholderEquity"])
+        marketPrice = si.get_live_price(comp)
+        shares = si.get_quote_data(comp)['sharesOutstanding']
+
+        bsCurrency = getBalanceSheetCurrency(comp)
+        listingCurrency = getListingCurrency(comp)
+        exRate = currency_getExchangeRate.getExchangeRate(exchange_rate_dict,
+                                                          listingCurrency, bsCurrency)
+
+        marketCap = marketPrice * shares
+        pb = marketCap / (equity / exRate)
+        pe = marketCap / (netIncome / exRate)
+
+        if pb < 1:
+            print(comp, ' pb < 1', pb)
+            continue
+
+        if 0 < pe < 10:
+            print(comp, ' 0< pe < 10')
+            continue
+
+        revenue = getFromDF(incomeStatement.loc["totalRevenue"])
+
+        retainedEarningsAssetRatio = retainedEarnings / totalAssets
+        cfoAssetRatio = cfo / totalAssets
+        ebitAssetRatio = ebit / totalAssets
+
+        data = si.get_data(comp, start_date=START_DATE, interval=PRICE_INTERVAL)
+        divs = si.get_dividends(comp, start_date=DIVIDEND_START_DATE)
+        percentile = 100.0 * (data['adjclose'][-1] - data['adjclose'].min()) / (
+                data['adjclose'].max() - data['adjclose'].min())
+        divSum = divs['dividend'].sum() if not divs.empty else 0
+
+        outputString = comp + " " + country.replace(" ", "_")[0:3] + " " \
+                       + sector.replace(" ", "_")[0:3] + " " \
+                       + listingCurrency + bsCurrency \
+                       + " MV:" + str(round(marketCap / 1000000000.0, 1)) + 'B' \
+                       + " PE " + str(round(pe, 2)) \
+                       + " Eq:" + str(round((totalAssets - totalLiab) / exRate / 1000000000.0, 1)) + 'B' \
+                       + " CR:" + str(round(currentRatio, 1)) \
+                       + " D/E:" + str(round(debtEquityRatio, 1)) \
+                       + " RE/A:" + str(round(retainedEarningsAssetRatio, 1)) \
+                       + " cfo/A:" + str(round(cfoAssetRatio, 1)) \
+                       + " ebit/A:" + str(round(ebitAssetRatio, 1)) \
+                       + " S/A " + str(round(revenue / totalAssets)) \
+                       + " pb:" + str(round(pb, 1)) \
+                       + " 52w p%: " + str(round(percentile)) \
+                       + " div10yr: " + str(round(divSum / marketPrice, 2))
+
+        print(outputString)
+        fileOutput.write(outputString + '\n')
+        fileOutput.flush()
+
     except Exception as e:
-        print(comp, "exception on info or BS")
-    else:
-        if (country.lower()) == "china":
-            print(comp, "NO CHINA")
-        else:
-            try:
-                cf = si.get_cash_flow(comp)
-                incomeStatement = si.get_income_statement(comp, yearly=True)
+        print(comp, "exception", e)
 
-                equity = getFromDF(bs.loc["totalStockholderEquity"])
-                totalCurrentAssets = getFromDF(bs.loc["totalCurrentAssets"])
-                totalCurrentLiab = getFromDF(bs.loc["totalCurrentLiabilities"])
-                totalAssets = getFromDF(bs.loc["totalAssets"])
-                totalLiab = getFromDF(bs.loc["totalLiab"])
-                retainedEarnings = getFromDF(bs.loc["retainedEarnings"])
-
-                # IS
-                revenue = getFromDF(incomeStatement.loc["totalRevenue"])
-                ebit = getFromDF(incomeStatement.loc["ebit"])
-                netIncome = getFromDF(incomeStatement.loc['netIncome'])
-
-                # CF
-                cfo = getFromDF(cf.loc["totalCashFromOperatingActivities"])
-                # print("cfo ", cfo, cf.loc["totalCashFromOperatingActivities"])
-                # cfi = cf.loc["totalCashflowsFromInvestingActivities"][0]
-                # cff = cf.loc["totalCashFromFinancingActivities"][0]
-                marketPrice = si.get_live_price(comp)
-                shares = si.get_quote_data(comp)['sharesOutstanding']
-
-                bsCurrency = getBalanceSheetCurrency(comp)
-                listingCurrency = getListingCurrency(comp)
-                exRate = currency_getExchangeRate.getExchangeRate(exchange_rate_dict,
-                                                                  listingCurrency, bsCurrency)
-
-            except Exception as e:
-                print("error when getting data ", comp, e)
-            else:
-                marketCap = marketPrice * shares
-                pb = marketCap / (equity / exRate)
-                pe = marketCap / (netIncome / exRate)
-                currentRatio = totalCurrentAssets / totalCurrentLiab
-                debtEquityRatio = totalLiab / (totalAssets - totalLiab)
-                retainedEarningsAssetRatio = retainedEarnings / totalAssets
-                cfoAssetRatio = cfo / totalAssets
-                ebitAssetRatio = ebit / totalAssets
-
-                try:
-                    assert currentRatio > 1, 'current ratio needs to be bigger than one:' + str(currentRatio)
-                    assert debtEquityRatio < 1, 'debt equity ratio needs to be less than one:' + \
-                                                str(debtEquityRatio)
-                    assert retainedEarnings > 0, "retained earnings needs to be greater than 0:" + \
-                                                 str(retainedEarnings)
-                    assert cfo > 0, "cfo needs to be greater than 0:" + str(cfo)
-                    assert ebit > 0, "ebit needs to be postive:" + str(ebit)
-                    assert netIncome > 0, "net income needs to be positive:" + str(netIncome)
-                    assert pb < 1, "pb needs to be < 1:" + str(pb)
-                    assert (0 < pe < 10), "pb needs to between 0 and 10:" + str(pe)
-                except AssertionError as ae:
-                    print(datetime.datetime.now().time().strftime("%H:%M"), comp, "fails assertion", ae)
-                else:
-                    if (currentRatio > 1 and debtEquityRatio < 1 and retainedEarnings > 0
-                            and cfo > 0 and ebit > 0 and netIncome > 0):
-                        try:
-                            data = si.get_data(comp, start_date=START_DATE, interval=PRICE_INTERVAL)
-                            divs = si.get_dividends(comp, start_date=DIVIDEND_START_DATE)
-                            percentile = 100.0 * (data['adjclose'][-1] - data['adjclose'].min()) / (
-                                    data['adjclose'].max() - data['adjclose'].min())
-                            divSum = divs['dividend'].sum() if not divs.empty else 0
-
-                        except Exception as e:
-                            print(comp, "exception issue ", e)
-                        else:
-                            outputString = comp + " " + country.replace(" ", "_")[0:3] + " " \
-                                           + sector.replace(" ", "_")[0:3] + " " \
-                                           + listingCurrency + bsCurrency \
-                                           + " MV:" + str(round(marketCap / 1000000000.0, 1)) + 'B' \
-                                           + " PE " + str(round(pe, 2)) \
-                                           + " Eq:" + str(
-                                round((totalAssets - totalLiab) / exRate / 1000000000.0, 1)) + 'B' \
-                                           + " CR:" + str(round(currentRatio, 1)) \
-                                           + " D/E:" + str(round(debtEquityRatio, 1)) \
-                                           + " RE/A:" + str(round(retainedEarningsAssetRatio, 1)) \
-                                           + " cfo/A:" + str(round(cfoAssetRatio, 1)) \
-                                           + " ebit/A:" + str(round(ebitAssetRatio, 1)) \
-                                           + " S/A " + str(round(revenue / totalAssets)) \
-                                           + " pb:" + str(round(pb, 1)) \
-                                           + " 52w p%: " + str(round(percentile)) \
-                                           + " div10yr: " + str(round(divSum / marketPrice, 2))
-
-                            print(outputString)
-                            fileOutput.write(outputString + '\n')
-                            fileOutput.flush()
+    # else:
+    #     try:
+    #
+    #         # IS
+    #
+    #         # print("cfo ", cfo, cf.loc["totalCashFromOperatingActivities"])
+    #         # cfi = cf.loc["totalCashflowsFromInvestingActivities"][0]
+    #         # cff = cf.loc["totalCashFromFinancingActivities"][0]
+    #
+    #
+    #     except Exception as e:
+    #         print("error when getting data ", comp, e)
+    #     else:
+    #
+    #
+    #
+    #         # try:
+    #         #     # assert currentRatio > 1, 'current ratio needs to be bigger than one:' + str(currentRatio)
+    #         #     # assert debtEquityRatio < 1, 'debt equity ratio needs to be less than one:' + \
+    #         #     #                             str(debtEquityRatio)
+    #         #     # assert retainedEarnings > 0, "retained earnings needs to be greater than 0:" + \
+    #         #     #                              str(retainedEarnings)
+    #         #     # assert cfo > 0, "cfo needs to be greater than 0:" + str(cfo)
+    #         #     # assert ebit > 0, "ebit needs to be postive:" + str(ebit)
+    #         #     # assert netIncome > 0, "net income needs to be positive:" + str(netIncome)
+    #         #     assert pb < 1, "pb needs to be < 1:" + str(pb)
+    #         #     assert (0 < pe < 10), "pb needs to between 0 and 10:" + str(pe)
+    #         # except AssertionError as ae:
+    #         #     print(datetime.datetime.now().time().strftime("%H:%M"), comp, "fails assertion", ae)
+    #         # else:
+    #         # if (currentRatio > 1 and debtEquityRatio < 1 and retainedEarnings > 0
+    #         #         and cfo > 0 and ebit > 0 and netIncome > 0):
+    #         try:
+    #
+    #
+    #         except Exception as e:
+    #             print(comp, "exception issue ", e)
+    #         else:
