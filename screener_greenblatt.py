0796b388f99ce3b2fa4210bb209b22fc6438bc68
Luke <shijiluke@gmail.com>
2022-02-07 09:20:54 +0800

    2 7 0920

---
+++ screener_greenblatt.py
@@ -0,0 +1,98 @@
+# maximize roe/pe
+
+import yahoo_fin.stock_info as si
+import pandas as pd
+from currency_scrapeYahoo import getBalanceSheetCurrency
+from currency_scrapeYahoo import getListingCurrency
+import currency_getExchangeRate
+from helperMethods import getFromDF
+
+COUNT = 0
+
+
+def increment():
+    global COUNT
+    COUNT = COUNT + 1
+    return COUNT
+
+
+START_DATE = '3/1/2020'
+DIVIDEND_START_DATE = '1/1/2010'
+# PRICE_INTERVAL = '1mo'
+
+exchange_rate_dict = currency_getExchangeRate.getExchangeRateDict()
+
+fileOutput = open('list_greenblatt', 'w')
+fileOutput.write("\n")
+
+stock_df = pd.read_csv('list_companyInfo', sep="\t", index_col=False,
+                       names=['ticker', 'name', 'sector', 'industry', 'country', 'mv', 'price'])
+
+listStocks = stock_df[(stock_df['price'] > 1)
+                      & (stock_df['country'].str.lower() != 'china')]['ticker'].tolist()
+
+print(len(listStocks), listStocks)
+
+for comp in listStocks:
+    print(increment())
+    try:
+        marketPrice = si.get_live_price(comp)
+
+        # info = si.get_company_info(comp)
+        # if info.loc["country"][0].lower() == "china":
+        #     print(comp, "no china")
+        #     continue
+
+        bs = si.get_balance_sheet(comp)
+        totalCurrentAssets = getFromDF(bs.loc["totalCurrentAssets"])
+        totalCurrentLiab = getFromDF(bs.loc["totalCurrentLiabilities"])
+        currentRatio = totalCurrentAssets / totalCurrentLiab
+
+        if currentRatio < 1:
+            print(comp, "current ratio < 1")
+            continue
+
+        totalAssets = getFromDF(bs.loc["totalAssets"])
+        totalLiab = getFromDF(bs.loc["totalLiab"])
+        debtEquityRatio = totalLiab / (totalAssets - totalLiab)
+
+        if debtEquityRatio > 1:
+            print(comp, " de ratio> 1. ", debtEquityRatio)
+            continue
+
+        incomeStatement = si.get_income_statement(comp, yearly=True)
+        netIncome = getFromDF(incomeStatement.loc['netIncome'])
+
+        roe = netIncome / totalAssets
+
+        equity = getFromDF(bs.loc["totalStockholderEquity"])
+        shares = si.get_quote_data(comp)['sharesOutstanding']
+
+        bsCurrency = getBalanceSheetCurrency(comp)
+        listingCurrency = getListingCurrency(comp)
+        exRate = currency_getExchangeRate.getExchangeRate(exchange_rate_dict,
+                                                          listingCurrency, bsCurrency)
+
+        marketCap = marketPrice * shares
+        pe = marketCap / (netIncome / exRate)
+
+        data = si.get_data(comp, start_date=START_DATE, interval=PRICE_INTERVAL)
+        divs = si.get_dividends(comp, start_date=DIVIDEND_START_DATE)
+        percentile = 100.0 * (data['adjclose'][-1] - data['adjclose'].min()) / (
+                data['adjclose'].max() - data['adjclose'].min())
+        divSum = divs['dividend'].sum() if not divs.empty else 0
+
+        outputString = comp + " " \
+                       + stock_df[stock_df['ticker'] == 'M'][['country', 'sector']] \
+                           .to_string(index=False, header=False) + " " \
+                       + listingCurrency + bsCurrency \
+                       + " ROE:" + str(round(roe * 100, 2)) \
+                       + " PE:" + str(round(pe, 1)) \
+                       + " ROE/PE:" + str(round(roe * 100 / pe, 2))
+
+        print(outputString)
+        fileOutput.write(outputString + '\n')
+        fileOutput.flush()
+
+    except Exception as e:
+        print(comp, "exception", e)
