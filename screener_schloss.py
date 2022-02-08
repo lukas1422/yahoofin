a6c6a0faa1e5197e883828995431feecf1b7033f
Luke <shijiluke@gmail.com>
2022-02-08 16:10:52 +0800

    2 8 1610

--- screener_schloss.py
+++ screener_schloss.py
@@ -1,4 +1,9 @@
-# schloss method, no financials, low PB, low debt, 52 week low
+# schloss method
+# no financials,
+# PB < 1
+# no long term debt
+# price < 1.1 * 52 week low
+# insider  ownership > median
 
 import yahoo_fin.stock_info as si
 import pandas as pd
@@ -24,6 +29,13 @@ exchange_rate_dict = currency_getExchangeRate.getExchangeRateDict()
 
 fileOutput = open('list_schlossOutput', 'w')
 
+ownership = pd.read_csv('list_insiderOwnership_finviz', sep=' ',
+                        index_col=False, names=['ticker', 'perc'])
+ownership['perc'] = ownership['perc'].replace('-', '0')
+ownership['perc'] = ownership['perc'].str.rstrip("%").astype(float)
+ownershipDic = pd.Series(ownership.perc.values, index=ownership.ticker).to_dict()
+print(ownershipDic)
+
 stock_df = pd.read_csv('list_companyInfo', sep="\t", index_col=False,
                        names=['ticker', 'name', 'sector', 'industry', 'country', 'mv', 'price'])
 
@@ -33,11 +45,16 @@ listStocks = stock_df[(stock_df['price'] > 1)
                       & (stock_df['industry'].str.contains('reit', regex=True, case=False) == False)
                       & (stock_df['country'].str.lower() != 'china')]['ticker'].tolist()
 
-print(len(listStocks), listStocks)
+# print(len(listStocks), listStocks)
 
 for comp in listStocks:
     print(increment())
     try:
+        insiderPerc = ownershipDic[comp]
+        if insiderPerc < 10:
+            print(comp, "insider ownership < 10%", insiderPerc)
+            continue
+
         marketPrice = si.get_live_price(comp)
         if marketPrice < 1:
             print(comp, 'market price < 1: ', marketPrice)
@@ -65,7 +82,10 @@ for comp in listStocks:
 
         totalAssets = getFromDF(bs.loc["totalAssets"])
         totalLiab = getFromDF(bs.loc["totalLiab"])
+        currentLiab = getFromDF(bs.loc['totalCurrentLiabilities'])
         debtEquityRatio = totalLiab / (totalAssets - totalLiab)
+
+        longTermDebtRatio = (totalLiab - currentLiab) / totalAssets
 
         if debtEquityRatio > 1 or totalAssets < totalLiab:
             print(comp, " de ratio> 1 or A<L. ", debtEquityRatio)
@@ -87,6 +107,10 @@ for comp in listStocks:
         #     continue
 
         equity = getFromDF(bs.loc["totalStockholderEquity"])
+
+        if equity < 0:
+            print(comp, "equity < 0", equity)
+
         shares = si.get_quote_data(comp)['sharesOutstanding']
 
         bsCurrency = getBalanceSheetCurrency(comp)
@@ -115,14 +139,10 @@ for comp in listStocks:
         data = si.get_data(comp, start_date=START_DATE, interval=PRICE_INTERVAL)
         divs = si.get_dividends(comp, start_date=DIVIDEND_START_DATE)
         low_52wk = data['adjclose'].min()
-        # percentile = 100.0 * (data['adjclose'][-1] - data['adjclose'].min()) / (
-        #         data['adjclose'].max() - data['adjclose'].min())
 
         if marketPrice > low_52wk * 1.1:
             print(comp, "exceeding 52wk low * 1.1")
             continue
-
-        # divSum = divs['dividend'].sum() if not divs.empty else 0
 
         outputString = comp + " " \
                        + stock_df[stock_df['ticker'] == comp][['country', 'sector']] \
@@ -131,7 +151,9 @@ for comp in listStocks:
                        + " MV:" + str(round(marketCap / 1000000000.0, 1)) + 'B' \
                        + " Eq:" + str(round((totalAssets - totalLiab) / exRate / 1000000000.0, 1)) + 'B' \
                        + " D/E:" + str(round(debtEquityRatio, 1)) \
+                       + " LT debt ratio" + str(round(longTermDebtRatio, 1)) \
                        + " pb:" + str(round(pb, 1)) \
+                       + " insider%:" + str(round(insiderPerc, 1)) \
                        + " p/52low: " + str(round(marketPrice / low_52wk))
 
         print(outputString)
