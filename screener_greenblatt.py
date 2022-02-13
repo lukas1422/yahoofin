6aeea446cf6368c6d471d547d36002e8fa8acfb3
Luke <shijiluke@gmail.com>
2022-02-13 10:37:41 +0800

    2 13 1037

--- screener_greenblatt.py
+++ screener_greenblatt.py
@@ -1,4 +1,4 @@
-# maximize roe/pe
+# maximize CFO/Asset/PB
 
 import yahoo_fin.stock_info as si
 import pandas as pd
@@ -27,24 +27,42 @@ fileOutput = open('list_greenblatt', 'w')
 stock_df = pd.read_csv('list_companyInfo', sep="\t", index_col=False,
                        names=['ticker', 'name', 'sector', 'industry', 'country', 'mv', 'price'])
 
-listStocks = stock_df[(stock_df['price'] > 1) & (stock_df['country'].str.lower() != 'china')]['ticker'].tolist()
+# listStocks = stock_df[(stock_df['price'] > 1) & (stock_df['country'].str.lower() != 'china')]['ticker'].tolist()
+
+listStocks = stock_df[(stock_df['price'] > 1)
+                      & (stock_df['sector'].str
+                         .contains('financial|healthcare', regex=True, case=False) == False)
+                      # & (stock_df['ticker'].str.lower() > 'w')
+                      & (stock_df['industry'].str.contains('reit', regex=True, case=False) == False)
+                      & (stock_df['country'].str.lower() != 'china')]['ticker'].tolist()
 
 print(len(listStocks), listStocks)
 
 for comp in listStocks:
     print(increment())
     try:
-        incomeStatement = si.get_income_statement(comp, yearly=True)
-        netIncome = getFromDF(incomeStatement.loc['netIncome'])
 
-        if netIncome < 0:
-            print(comp, "net income < 0")
-            continue
+        cf = si.get_cash_flow(comp)
+        cfo = getFromDF(cf.loc["totalCashFromOperatingActivities"])
+        if cfo < 0:
+            print(comp, "cfo < 0")
+
+        # incomeStatement = si.get_income_statement(comp, yearly=True)
+        # netIncome = getFromDF(incomeStatement.loc['netIncome'])
+        #
+        # if netIncome < 0:
+        #     print(comp, "net income < 0")
+        #     continue
 
         bs = si.get_balance_sheet(comp)
         totalAssets = getFromDF(bs.loc["totalAssets"])
+        totalLiab = getFromDF(bs.loc["totalLiab"])
 
-        roa = netIncome / totalAssets
+        if totalAssets < totalLiab:
+            print(comp, "total A less than total L")
+            continue
+
+        # roa = netIncome / totalAssets
 
         shares = si.get_quote_data(comp)['sharesOutstanding']
 
@@ -54,15 +72,18 @@ for comp in listStocks:
 
         marketPrice = si.get_live_price(comp)
         marketCap = marketPrice * shares
-        pe = marketCap / (netIncome / exRate)
+        # pe = marketCap / (netIncome / exRate)
+        equity = totalAssets - totalLiab
+        pb = marketCap / (equity / exRate)
+        cfoAssetRatio = cfo / totalAssets
 
         outputString = comp + " " \
                        + stock_df[stock_df['ticker'] == comp][['country', 'sector']] \
                            .to_string(index=False, header=False) + " " \
                        + listingCurrency + bsCurrency \
-                       + " ROA:" + str(round(roa * 100, 2)) \
-                       + " PE:" + str(round(pe, 1)) \
-                       + " ROA/PE:" + str(round(roa * 100 / pe, 2))
+                       + " cfoAssetRatio:" + str(round(cfoAssetRatio * 100, 2)) \
+                       + " PB:" + str(round(pb, 1)) \
+                       + " cfoAsset/PB:" + str(round(cfoAssetRatio * 100 / pb, 2))
 
         print(outputString)
         fileOutput.write(outputString + '\n')
