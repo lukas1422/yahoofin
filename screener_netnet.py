8991c91232ab01e0a60f4273f7e9bb1f8e503b1d
Luke <shijiluke@gmail.com>
2022-02-17 18:33:44 +0800

    2 17 1833

--- screener_netnet.py
+++ screener_netnet.py
@@ -43,6 +43,7 @@ if MARKET == Market.US:
                           & (stock_df['listingDate'] < pd.to_datetime('2010-1-1'))
                           & (stock_df['industry'].str.contains('reit', regex=True, case=False) == False)
                           & (stock_df['country'].str.lower() != 'china')]['ticker'].tolist()
+
 elif MARKET == Market.HK:
     stock_df = pd.read_csv('list_hkstocks', dtype=object, sep=" ", index_col=False, names=['ticker', 'name'])
     stock_df['ticker'] = stock_df['ticker'].astype(str)
@@ -73,8 +74,8 @@ for comp in listStocks:
         retainedEarnings = getFromDF(bs.loc["retainedEarnings"]) if 'retainedEarnings' in bs.index else 0
 
         # RE>0 ensures that the stock is not a chronic cash burner
-        if retainedEarnings < 0:
-            print(comp, " retained earnings < 0 ", retainedEarnings)
+        if retainedEarnings <= 0:
+            print(comp, " retained earnings <= 0 ", retainedEarnings)
             continue
 
         currentAssets = getFromDF(bs.loc["totalCurrentAssets"]) if 'totalCurrentAssets' in bs.index else 0.0
@@ -90,79 +91,83 @@ for comp in listStocks:
         receivables = getFromDF(bs.loc['netReceivables']) if 'netReceivables' in bs.index else 0.0
         inventory = getFromDF(bs.loc['inventory']) if 'inventory' in bs.index else 0.0
 
-        # shares = scrape_sharesOutstanding.scrapeTotalSharesXueqiu(comp)
-        if MARKET == Market.US:
-            shares = si.get_quote_data(comp)['sharesOutstanding']
-        elif MARKET == Market.HK:
-            shares = hk_shares[hk_shares['ticker'] == comp]['shares'].values[0]
-        else:
-            raise Exception("version not found ", MARKET)
-
-        # print("shares ", shares)
-        marketCap = marketPrice * shares
-        # print("market cap ", marketCap)
-
-        listingCurr = getListingCurrency(comp)
-        bsCurr = getBalanceSheetCurrency(comp, listingCurr)
-        exRate = currency_getExchangeRate.getExchangeRate(exchange_rate_dict, listingCurr, bsCurr)
-
-        if (cash + receivables + inventory - totalLiab) / exRate < marketCap:
-            print(comp, listingCurr, bsCurr,
-                  'cash + rec + inventory - total liab < mv. cash rec inv Liab MV:',
-                  round(cash / 1000000000, 2),
-                  round(receivables / 1000000000, 2),
-                  round(inventory / 1000000000, 2),
-                  round(totalLiab / 1000000000, 2),
-                  round(marketCap / 1000000000, 2))
+        if cash == 0:
+            print(comp, 'cash is 0 ')
             continue
 
-        # outputString = ""
-        # if (cash - totalLiab) / exRate > marketCap:
-        #     outputString = "cash netnet:" + comp + " " \
-        #                    + listingCurr + bsCurr \
-        #                    + " cash:" + str(round(cash / 1000000000, 2)) \
-        #                    + " L:" + str(round(totalLiab / 1000000000, 2))
-        # elif (cash + receivables * 0.5 - totalLiab) / exRate > marketCap:
-        #     outputString = "cash receivable netnet:" + comp + " "
-        # elif (cash + receivables * 0.5 + inventory * 0.3 - totalLiab) / exRate > marketCap:
-        #     outputString = "cash rec inv netnet " + comp
-        # elif (currentAssets - totalLiab) / exRate > marketCap:
-        #     outputString = 'currentAsset netnet ' + comp
-        # else:
-        #     outputString = 'undefined net net,check:' + comp
+            # shares = scrape_sharesOutstanding.scrapeTotalSharesXueqiu(comp)
+            if MARKET == Market.US:
+                shares = si.get_quote_data(comp)['sharesOutstanding']
+            elif MARKET == Market.HK:
+                shares = hk_shares[hk_shares['ticker'] == comp]['shares'].values[0]
+            else:
+                raise Exception("version not found ", MARKET)
 
-        additionalComment = ""
-        if (cash - totalLiab) / exRate - marketCap > 0:
-            profit = (cash - totalLiab) / exRate - marketCap
-            additionalComment = " clean cash netnet, profit:" + str(round(profit, 2))
-        elif (cash + receivables - totalLiab) / exRate - marketCap > 0:
-            additionalComment = " receivable conversion rate required: " \
-                                + str(round((totalLiab + marketCap * exRate - cash) / receivables, 2))
-        elif (cash + 0.5 * receivables + inventory - totalLiab) / exRate - marketCap > 0:
-            additionalComment = " inventory conversion rate required: " \
-                                + str(round((totalLiab + marketCap * exRate - cash - 0.5 * receivables)
-                                            / inventory, 2))
+            # print("shares ", shares)
+            marketCap = marketPrice * shares
+            # print("market cap ", marketCap)
 
-        outputString = comp + " " + listingCurr + bsCurr \
-                       + country.replace(" ", "_") + " " \
-                       + sector.replace(" ", "_") + " " \
-                       + " cash:" + str(round(cash / 1000000000, 2)) \
-                       + " rec:" + str(round(receivables / 1000000000, 2)) \
-                       + " inv:" + str(round(inventory / 1000000000, 2)) \
-                       + " CA:" + str(round(currentAssets / 1000000000, 2)) \
-                       + " L:" + str(round(totalLiab / 1000000000, 2)) \
-                       + " mv:" + str(round(marketCap / 1000000000, 2)) \
-                       + additionalComment
+            listingCurr = getListingCurrency(comp)
+            bsCurr = getBalanceSheetCurrency(comp, listingCurr)
+            exRate = currency_getExchangeRate.getExchangeRate(exchange_rate_dict, listingCurr, bsCurr)
 
-        # + ' profit:' + str(round(((cash + 0.5 * receivables
-        #                            + 0.3 * inventory - totalLiab) / exRate - marketCap)
-        #                          / 1000000000, 2))
+            if (cash + receivables + inventory - totalLiab) / exRate < marketCap:
+                print(comp, listingCurr, bsCurr,
+                      'cash + rec + inventory - total liab < mv. cash rec inv Liab MV:',
+                      round(cash / 1000000000, 2),
+                      round(receivables / 1000000000, 2),
+                      round(inventory / 1000000000, 2),
+                      round(totalLiab / 1000000000, 2),
+                      round(marketCap / 1000000000, 2))
+                continue
 
-        print(outputString)
+            # outputString = ""
+            # if (cash - totalLiab) / exRate > marketCap:
+            #     outputString = "cash netnet:" + comp + " " \
+            #                    + listingCurr + bsCurr \
+            #                    + " cash:" + str(round(cash / 1000000000, 2)) \
+            #                    + " L:" + str(round(totalLiab / 1000000000, 2))
+            # elif (cash + receivables * 0.5 - totalLiab) / exRate > marketCap:
+            #     outputString = "cash receivable netnet:" + comp + " "
+            # elif (cash + receivables * 0.5 + inventory * 0.3 - totalLiab) / exRate > marketCap:
+            #     outputString = "cash rec inv netnet " + comp
+            # elif (currentAssets - totalLiab) / exRate > marketCap:
+            #     outputString = 'currentAsset netnet ' + comp
+            # else:
+            #     outputString = 'undefined net net,check:' + comp
 
-        fileOutput.write(outputString + '\n')
-        fileOutput.flush()
+            additionalComment = ""
+            if (cash - totalLiab) / exRate - marketCap > 0:
+                profit = (cash - totalLiab) / exRate - marketCap
+                additionalComment = " clean cash netnet, profit:" + str(round(profit, 2))
+            elif (cash + receivables - totalLiab) / exRate - marketCap > 0:
+                additionalComment = " receivable conversion rate required: " \
+                                    + str(round((totalLiab + marketCap * exRate - cash) / receivables, 2))
+            elif (cash + 0.5 * receivables + inventory - totalLiab) / exRate - marketCap > 0:
+                additionalComment = " inventory conversion rate required: " \
+                                    + str(round((totalLiab + marketCap * exRate - cash - 0.5 * receivables)
+                                                / inventory, 2))
+
+            outputString = comp + " " + listingCurr + bsCurr \
+                           + country.replace(" ", "_") + " " \
+                           + sector.replace(" ", "_") + " " \
+                           + " cash:" + str(round(cash / 1000000000, 2)) \
+                           + " rec:" + str(round(receivables / 1000000000, 2)) \
+                           + " inv:" + str(round(inventory / 1000000000, 2)) \
+                           + " CA:" + str(round(currentAssets / 1000000000, 2)) \
+                           + " L:" + str(round(totalLiab / 1000000000, 2)) \
+                           + " mv:" + str(round(marketCap / 1000000000, 2)) \
+                           + additionalComment
+
+            # + ' profit:' + str(round(((cash + 0.5 * receivables
+            #                            + 0.3 * inventory - totalLiab) / exRate - marketCap)
+            #                          / 1000000000, 2))
+
+            print(outputString)
+
+            fileOutput.write(outputString + '\n')
+            fileOutput.flush()
 
 
-    except Exception as e:
+        except Exception as e:
         print(comp, "exception", e)
