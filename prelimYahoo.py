09655f274cd89dfc68a02f8477d859465a0da83a
Luke <shijiluke@gmail.com>
2022-02-20 21:48:41 +0800

    2 20 2148

--- prelimYahoo.py
+++ prelimYahoo.py
@@ -17,7 +17,7 @@ def fo(number):
 exchange_rate_dict = currency_getExchangeRate.getExchangeRateDict()
 
 # stockName = '0001.HK'
-stockName = '1270.HK'
+stockName = '1194.HK'
 # stockName = 'VIAC'
 yearlyFlag = False
 
