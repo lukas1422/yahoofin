0a2baf2fe07252ada4c804f7afd23ce2e33620bd
Luke <shijiluke@gmail.com>
2022-03-07 12:37:26 +0800

    37

--- prelimYahoo.py
+++ prelimYahoo.py
@@ -19,7 +19,7 @@ def fo(number):
 
 exchange_rate_dict = currency_getExchangeRate.getExchangeRateDict()
 
-stockName = '6981.T'
+stockName = '3369.HK'
 yearlyFlag = False
 try:
     data = si.get_data(stockName, start_date=START_DATE, interval=PRICE_INTERVAL)
