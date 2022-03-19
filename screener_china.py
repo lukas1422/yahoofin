b96c9cd17f7fc57d4a3bc5d8b6004821e499043c
Luke <shijiluke@gmail.com>
2022-03-19 20:22:25 +0800

    3 19 2021

--- screener_china.py
+++ screener_china.py
@@ -125,7 +125,6 @@ for comp in listStocks:
 
         if shares == "error":
             shares = scrape_sharesOutstanding.scrapeTotalSharesXueqiu(convertChinaForXueqiu(comp[:6]))
-
             print(comp, 'share was error', shares)
 
         print(comp, 'shares', shares)
