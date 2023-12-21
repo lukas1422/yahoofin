da68237a7485d57e9bb7e04a1d16c5cbb3375600
lukas1422 <shijiluke@gmail.com>
2023-12-21 13:21:09 +0800

    1221

--- yfinanceTest.py
+++ yfinanceTest.py
@@ -31,7 +31,7 @@ exchange_rate_dict = currency_getExchangeRate.getExchangeRateDict()
 fileOutput = open('list_results_hk', 'w')
 
 stock_df = pd.read_csv('list_HK_Tickers', dtype=object, sep=" ", index_col=False,
-                       names=['ticker', 'name'],error_bad_lines=False)
+                       names=['ticker', 'name'], error_bad_lines=False)
 
 #
 stock_df['ticker'] = stock_df['ticker'].astype(str)
@@ -42,28 +42,27 @@ print(listStocks)
 
 # listStocks = ['2127.HK']
 
-#print(listStocks)
-fileOutput = open('list_results_hk', 'w')
+# print(listStocks)
+# fileOutput = open('list_results_hk', 'w')
 
 for s in listStocks:
     print(s)
     stock = yf.Ticker(s)
-    #inc = stock.income_stmt
+    # inc = stock.income_stmt
     try:
         bs = stock.balance_sheet
         cash = bs.loc['Cash And Cash Equivalents'][0]
         liab = bs.loc['Total Liabilities Net Minority Interest'][0]
         cap = stock.get_fast_info()['marketCap']
 
-        if((cash-liab)/cap>1):
-            outputString = (s + ' cash:'+ str(roundB(cash,2)) \
-                            + ' liab:' + str(roundB(liab,2)) + ' cap:' + str(roundB(cap,2)) \
-                            +' ratio:' + str(round((cash-liab)/cap,2)))
+        if ((cash - liab) / cap > 1):
+            outputString = (s + ' cash:' + str(roundB(cash, 2)) \
+                            + ' liab:' + str(roundB(liab, 2)) + ' cap:' + str(roundB(cap, 2)) \
+                            + ' ratio:' + str(round((cash - liab) / cap, 2)))
             print(outputString)
             fileOutput.write(outputString + '\n')
             fileOutput.flush()
 
     except Exception as e:
-        print(s,e)
+        print(s, e)
         continue
-
